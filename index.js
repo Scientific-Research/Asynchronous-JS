9e41543f1ad53b94aba684cb06799e1a8002a6c5
Maximilian <scientificresearch309@gmail.com>
2024-02-15 12:47:53 +0100

    I added the error message with return in case of having error message in both call backs functions

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
     // res.end(data);
+    // Return word here is very important to prevent the compiler to go further. Just Return!
+    if (err) return console.log(err.message);
+    // because of the Retuen, the Compiler will stop in this line and will back to the parent
+    // function(fs.readFile), that's why we will not have another error down again!
     console.log(res.body);
     // res.end(res.body.message);
     // console.log(res.body.message);
     // console.log(res.body.status);
     fs.writeFile(`${__dirname}/dog-written.txt`, `${res.body.message}`, 'utf-8', (err) => {
+      if (err) return console.log(err.message);
       console.log(`Random dog image saved to the file: ${res.body.message}`);
     });
   });
