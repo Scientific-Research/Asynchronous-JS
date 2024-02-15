7c4a175490ff4a249105416987bdd4b1b9ecb7fb
Maximilian <scientificresearch309@gmail.com>
2024-02-15 11:57:11 +0100

    we are fetching a message and status from this dog website!

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const fs = require("fs");
-const http = require("http");
+const fs = require('fs');
+const superagent = require('superagent');
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
-    res.end(data);
+// const server = http.createServer((req, res) => {
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+    // res.end(data);
+    console.log(res.body);
   });
-  /* this is our callback function:(err, data) => {
+});
+/* this is our callback function:(err, data) => {
     console.log(`The data is: ${data}`); which will be called as soon as
     our data reading is done! */
-});
+// });
 
-const PORT = 8000;
-server.listen(PORT, "127.0.0.1", () => {
-  console.log(`Server is listening to the PORT ${PORT}`);
-});
+// const PORT = 8000;
+// server.listen(PORT, "127.0.0.1", () => {
+//   console.log(`Server is listening to the PORT ${PORT}`);
+// });
