63c220c520a356b9d9d82b5c121b912251da6f75
Maximilian <scientificresearch309@gmail.com>
2024-02-15 10:52:13 +0100

    The complete Server program is there now and works well as it expected!

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
+const http = require("http");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+const server = http.createServer((req, res) => {
+  fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    console.log(`Breed: ${data}`);
+    res.end(data);
+  });
+  /* this is our callback function:(err, data) => {
+    console.log(`The data is: ${data}`); which will be called as soon as
+    our data reading is done! */
 });
-/* this is our callback function:(err, data) => {
-  console.log(`The data is: ${data}`); which will be called as soon as
-  our data reading is done! */
+
+const PORT = 8000;
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Server is listening to the PORT ${PORT}`);
+});
